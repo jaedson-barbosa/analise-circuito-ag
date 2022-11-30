a65821032d42b832379b630e7e7162b41fe22b5e
Jaedson <jaedson33@gmail.com>
2022-11-30 17:30:32 -0300

    Feitas melhorias.

--- index.py
+++ index.py
@@ -19,25 +19,23 @@ def simulate(r1: float, r2: float):
 
 
 # genetic algorithm search for continuous function optimization
-from numpy.random import randint
-from numpy.random import rand
+from numpy.random import randint, rand, choice
+from numpy import divide, sum
+from functools import reduce
  
 # objective function
 def objective(x):
-    return -simulate(x[0], x[1])
+    return simulate(x[0], x[1])
  
 # decode bitstring to numbers
 def decode(bounds, n_bits, bitstring):
 	decoded = list()
-	largest = 2**n_bits
+	largest = 2**n_bits-1
 	for i in range(len(bounds)):
 		# extract the substring
-		start, end = i * n_bits, (i * n_bits)+n_bits
-		substring = bitstring[start:end]
-		# convert bitstring to a string of chars
-		chars = ''.join([str(s) for s in substring])
-		# convert string to integer
-		integer = int(chars, 2)
+		start, end = i * n_bits, (i + 1) * n_bits
+		# convert bitarray to integer
+		integer = reduce(lambda a, b: a * 2 + b, bitstring[start:end])
 		# scale integer to desired range
 		value = bounds[i][0] + (integer/largest) * (bounds[i][1] - bounds[i][0])
 		# store
@@ -45,13 +43,22 @@ def decode(bounds, n_bits, bitstring):
 	return decoded
  
 # tournament selection
-def selection(pop, scores, k=3):
+def tournament_selection(pop, scores):
 	# first random selection
 	selection_ix = randint(len(pop))
-	for ix in randint(0, len(pop), k-1):
+	for ix in randint(0, len(pop), 2):
 		# check if better (e.g. perform a tournament)
-		if scores[ix] < scores[selection_ix]:
+		if scores[ix] > scores[selection_ix]:
 			selection_ix = ix
+	return pop[selection_ix]
+
+# roulette selection
+def roulette_selection(pop, scores):
+	# first normalize scores
+	scores = divide(scores, sum(scores))
+	# then choose a random index based on the probability vector
+	selection_ix = choice(len(pop), p=scores)
+	# finally returns the element from that index
 	return pop[selection_ix]
  
 # crossover two parents to create two children
@@ -76,24 +83,31 @@ def mutation(bitstring, r_mut):
 			bitstring[i] = 1 - bitstring[i]
  
 # genetic algorithm
-def genetic_algorithm(objective, bounds, n_bits, n_iter, n_pop, r_cross, r_mut):
+def genetic_algorithm(objective, bounds, n_bits, target, n_pop, r_cross, r_mut):
+	# number of generations needed to find the answer
+	n_iter = 0
+	# history of best scores
+	scores_history = list()
 	# initial population of random bitstring
 	pop = [randint(0, 2, n_bits*len(bounds)).tolist() for _ in range(n_pop)]
 	# keep track of best solution
 	best, best_eval = 0, objective(decode(bounds, n_bits, pop[0]))
 	# enumerate generations
-	for gen in range(n_iter):
+	while best_eval < target:
+		n_iter += 1
 		# decode population
 		decoded = [decode(bounds, n_bits, p) for p in pop]
 		# evaluate all candidates in the population
 		scores = [objective(d) for d in decoded]
 		# check for new best solution
-		for i in range(n_pop):
-			if scores[i] < best_eval:
-				best, best_eval = pop[i], scores[i]
-				print(">%d, new best f(%s) = %f" % (gen,  decoded[i], scores[i]))
+		scores_best = max(scores)
+		scores_history.append(scores_best)
+		if scores_best > best_eval:
+			i = scores.index(scores_best)
+			best, best_eval = pop[i], scores[i]
+			print(">%d, new best f(%s) = %f" % (n_iter,  decoded[i], scores[i]))
 		# select parents
-		selected = [selection(pop, scores) for _ in range(n_pop)]
+		selected = [roulette_selection(pop, scores) for _ in range(n_pop)]
 		# create the next generation
 		children = list()
 		for i in range(0, n_pop, 2):
@@ -107,24 +121,23 @@ def genetic_algorithm(objective, bounds, n_bits, n_iter, n_pop, r_cross, r_mut):
 				children.append(c)
 		# replace population
 		pop = children
-	return [best, best_eval]
+	best_decoded = decode(bounds, n_bits, best)
+	return [best_decoded, best_eval, scores_history]
 
 # define range for resistor
 r_range = [0, 22_000_000]
 # define range for input
 bounds = [r_range, r_range]
 # define the total iterations
-n_iter = 20
+target = 1.999
 # bits per variable
-n_bits = 16
+n_bits = 8
 # define the population size
-n_pop = 100
+n_pop = 20
 # crossover rate
 r_cross = 0.9
 # mutation rate
-r_mut = 1.0 / (float(n_bits) * len(bounds))
+r_mut = 1 / (float(n_bits) * len(bounds))
 # perform the genetic algorithm search
-best, score = genetic_algorithm(objective, bounds, n_bits, n_iter, n_pop, r_cross, r_mut)
-print('Done!')
-decoded = decode(bounds, n_bits, best)
-print('f(%s) = %f' % (decoded, -score))
+decoded, score, history = genetic_algorithm(objective, bounds, n_bits, target, n_pop, r_cross, r_mut)
+print('Done: f(%s) = %f' % (decoded, score))
